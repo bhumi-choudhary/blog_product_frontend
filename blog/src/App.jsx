22f9e2239c8d4427c4e0f29b97c8abb878718f59
ankit-jangir <ankitjangir5824@gmail.com>
2025-08-21 14:04:35 +0530

    forntend chnages

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Blog from './component/Blog';
+import User from './component/User';
 
 function App() {
 
 
   return (
     <>
-      <Blog />
+      {/* <Blog /> */}
+      <User/>
     </>
   )
 }
